54eb8dad936aef65e81e0296eb4780b4f5268f91
shejaemeric2 <shejaemeric2@gmail.com>
2023-03-12 17:38:08 +0200

    Create custom hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import useWindowResize from "./hooks/useWindowResize";
+import useFetch from "./hooks/useFetch";
 
-function App() {
+export default function App() {
+  const { windowHeight, windowWidth } = useWindowResize();
+  const [url, setUrl] = useState("");
+  const { loading, data, error } = useFetch(url);
+
+  // function to handle the form submission
+  function handleSubmit(event) {
+    event.preventDefault();
+    setUrl(event.target.url.value);
+  }
+
+  // update the URL state if the URL prop changes
+  useEffect(() => {
+    if (url) {
+      setUrl(url);
+    }
+  }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Assignment 1</h1>
+      <p>
+        Height: <span style={{ fontWeight: 800 }}>{windowHeight}</span>
+      </p>
+      <p>
+        Width: <span style={{ fontWeight: 800 }}>{windowWidth}</span>
+      </p>
+      <h1>Assignment 2</h1>
+      <div>
+        {/* Form for user to input URL */}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="url" placeholder="Enter URL..." />
+          <button type="submit">Fetch Data</button>
+        </form>
+
+        {/* Display API fetch state */}
+        <div>
+          <p>loading: {loading ? "true" : "false"}</p>
+          <p>data:{data ? JSON.stringify(data) : "{}"}</p>
+          <p>error: {error ? JSON.stringify(error) : "{}"}</p>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
